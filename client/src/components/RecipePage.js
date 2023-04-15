f0ed1f04911c63c0127a1c393ccab973513c9f19
Isabella <isabellapersaud041600@gmail.com>
2023-04-15 00:51:54 -0400

    added images for recipes

--- client/src/components/RecipePage.js
+++ client/src/components/RecipePage.js
@@ -1,30 +1,71 @@
+// import React, { useState } from "react";
+// import Recipe from "./Recipe";
+// import { Card, Image, Button, Header, Icon, Label } from "semantic-ui-react";
+// import { Link } from "react-router-dom";
+
+//     const linkStyle = {
+//     textDecoration: "none"
+//     };
+
+
+// function RecipePage({ recipes }) {
+
+
+    
+// return (
+//     <div className="card" style={{ display: "flex", flexWrap: "wrap" }}>
+//         {recipes.map(recipe => (
+//             <Link to={`/recipes/${recipe.id}`} key={recipe.id} style={linkStyle}>
+//                 <Card style={{ flex: "0 0 25%", margin: "8px" }}>
+//                     <img src={recipe.image} alt={recipe.title} className="recipe-image" />
+//                     <Card.Content>
+//                         <Card.Header>{recipe.title}</Card.Header>
+//                     </Card.Content>
+//                 </Card>
+//             </Link>
+//         ))}
+//     </div>
+// );
+// }
+// export default RecipePage;
+
+
+
 import React, { useState } from "react";
 import Recipe from "./Recipe";
 import { Card, Image, Button, Header, Icon, Label } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-    const linkStyle = {
+const linkStyle = {
     textDecoration: "none"
     };
 
-
-function RecipePage({ recipes }) {
-
-
-    
-return (
-    <div className="card" style={{ display: "flex", flexWrap: "wrap" }}>
+    function RecipePage({ recipes }) {
+    return (
+        <div className="card" style={{ display: "flex", flexWrap: "wrap" }}>
         {recipes.map(recipe => (
             <Link to={`/recipes/${recipe.id}`} key={recipe.id} style={linkStyle}>
-                <Card style={{ flex: "0 0 25%", margin: "8px" }}>
-                    <img src={recipe.image} alt={recipe.title} className="recipe-image" />
-                    <Card.Content>
-                        <Card.Header>{recipe.title}</Card.Header>
-                    </Card.Content>
-                </Card>
+            <Card
+                style={{
+                flex: "0 0 25%",
+                margin: "8px",
+                height: "300px",
+                borderRadius: "0"
+                }}
+            >
+                <Image
+                src={recipe.image}
+                alt={recipe.title}
+                style={{  display: "flex", height: "300px", objectFit: "cover", borderRadius: "0" }}
+                />
+                <Card.Content>
+                <Card.Header>{recipe.title}</Card.Header>
+                </Card.Content>
+            </Card>
             </Link>
         ))}
-    </div>
-);
+        </div>
+    );
 }
+
 export default RecipePage;
