10f92169f638f50cede1ac19807fd8611c25ce3c
Isabella <isabellapersaud041600@gmail.com>
2023-04-18 20:26:05 -0400

    card hover is working again

--- client/src/components/RecipePage.js
+++ client/src/components/RecipePage.js
@@ -8,7 +8,7 @@ function RecipePage({ recipes }) {
         <div className="card-wrapper">
         {recipes.map((recipe) => (
             <Link to={`/recipes/${recipe.id}`} key={recipe.id} className="link">
-            <Card className="card" id={`recipe-card-${recipe.id}`}> 
+            <Card className="recipe-card" id={`recipe-card-${recipe.id}`}> 
                 <Image src={recipe.image} alt={recipe.title} className="image" />
                 <Card.Content className="content">
                 <Card.Header
